a6ba86682e6362d3cfb899eaf72ed5fc9c5064bc
Diogo Ferreira <diogodanielsoaresferreira@ua.pt>
2018-09-09 18:50:22 +0100

    Reupload files to repository

---
+++ scripts/filewidget.js
@@ -0,0 +1,250 @@
+$(document).ready(function(){
+	$("#Labi").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/6cl7mhnjzui1pmlcfe3ffztriy7rl3vi?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#P1").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/yjprx51vt7v1szy13fvpa50wcheofip2?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#ISD").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/vsa8lsfm2ihnn0bnsj4z4bdcujeea6en?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#C1").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/4gfees8j7r1y75yzd8pehlycwbibsqwb?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#ALGA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/9n1qbtntxxgo7c657o0np0lv6v7tchql?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#P2").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/vp4fnj81fdzmy32z7vtt1g8r94x337mm?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#LSD").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/1lc3t5hyqj18mo14a0k5w53aejzu8kgx?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#C2").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/g4bizgna7kmnvbf0nc43ha00xzsuu4n9?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#MD").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/09kxld8s6rvgp2fovzjn0j4cmsd8n2ia?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#AC1").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/w9acujemzo9bz3r8876iyixgnvqwza9y?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#P3").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/iizz34yif86rzqm73nokwm6ox1n04x9g?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#MCE").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/1oxyokf30mkhn3hc4hih61vyvh585tt3?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#MPEI").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/jxx0wwu9b3jvakv4na0sgeepezahmwki?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#LFA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/uu3cgt16xudx06noaf5uw1rq7jovar8u?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#ACII").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/086ytd5nb9d3u7t38p65nkwft1gpphd8?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#ALGC").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/ctphsc2ar2rtqa25tawi5zyvrmfg2ud4?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#SE").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/bm8sve3kkcgj04twcb9mcf015sjgso67?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#SO").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/49db2hw4tw520xusqfz4hp8bri4a6m6y?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#AMS").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/kmwddd9at5ipwsmyozpgawdxr3wxha0u?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#IAA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/o79yy4fprpfcj3lswmnwca3s502rem74?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#FR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/scnzaqq4k67q8466e073rhuhi2za60m9?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#BD").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/gv0c41qrntj2vddr7tge59hiszqp6tc2?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#AR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/v4s3pa34fturavk24urkogjauz748idt?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#IHC").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/fbyphh4uq904o0q0hgetiaqpcba9b6e4?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#PEI").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed_widget/s/bdmt9ed8cknw2t7tkrih9ekupiyj2uaf?view=icon&sort=name&direction=ASC&theme=gray\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen> </iframe>");
+	});
+	$("#ACA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/y6uxl3ggwr1qqqzfm3x20siddk7rq46i?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#ARA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/ayu3jthe0fbjfxat34fu8b6lonxw3ol0?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#CV").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/tj6nme94behvktjo4nxc9ao7sjs1d43q?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#EDC").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/flvlwvrzn8pzrhmbybha7eiumm0juxz4?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#SEG").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/0qmtxdh22gq3fzyt9ihhg2q44oxh37pt?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#CR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/lr51jrn6ob5ti6yw7z0iv2e3bhij9nf7?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#DDR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/5ijew0163lsk73jpzbaqlb0gtb51i01b?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#ES").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/0sd9i4bhomhy3gdlaj7z6oulgn4wv6jb?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#SD").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/ijx10jblfkcvxxox9enw8j4dhk8sdvp6?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#TPR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/u1ish145he5ie48ruxptwx4qr4vyikcl?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#AA").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/kg3rtwuktn59wrvlb7rt4hkexay9v7xr?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#SAR").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/by32frlavfh1l320f8i9r9tjt4in31ng?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#GPE").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/57tgftzbgoe1q9kgtqxyfi4yr0hk34vw?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#ESe").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/fcfks5h9kel1hkqd28eaizn0vdi9qpdg?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#RI").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append("<iframe id=\"added\" src=\"https://app.box.com/embed/s/g8813sh3ifou5xn6eh4fytxs5ws19dtg?sortColumn=date&view=icon\" width=\"800\" height=\"550\" frameborder=\"0\" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>");
+	});
+	$("#contact").click(function(event){
+		$('.menu').remove();
+		$('#added').remove();
+		$('#mailform').remove();
+		$('#filew').append(
+		"<div id=\"mailform\"><p>Se por qualquer razão desejares entrar em contacto, não hesites em preencher formulário abaixo.</p><a id=\"foxyform_embed_link_146723\" href=\"http://br.foxyform.com/\">foxyform</a><script type=\"text/javascript\">(function(d, t){var g = d.createElement(t),s = d.getElementsByTagName(t)[0];g.src = \"http://br.foxyform.com/js.php?id=146723&sec_hash=98bd9203b5b&width=350px\";s.parentNode.insertBefore(g, s);}(document, \"script\"));</script></div>"
+		);
+	});
+});
---
+++ scripts/sidebarjs.js
@@ -0,0 +1,11 @@
+$(function () {
+
+	var links = $('.sidebar-links > div');
+
+	links.on('click', function () {
+
+		links.removeClass('selected');
+		$(this).addClass('selected');
+
+	});
+});
